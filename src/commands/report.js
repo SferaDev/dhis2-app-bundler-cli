2b15322f27229062457ef523552c0cb70acf3fbf
Alexis Rico <sferadev@gmail.com>
2022-06-13 10:42:52 +0200

    Update bundle code
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- src/commands/report.js
+++ src/commands/report.js
@@ -1,25 +1,38 @@
 const gulp = require("gulp");
 const inlineSource = require("gulp-inline-source-html");
 const replace = require("gulp-replace");
-const { execSync } = require("child_process");
-var concat = require("gulp-concat");
+const { exec } = require('@dhis2/cli-helpers-engine')
+const concat = require("gulp-concat");
+const concatCss = require('gulp-concat-css');
 
 const handler = async () => {
     const baseUrl = process.env.DHIS2_BASE_URL || "..";
-    execSync("d2-app-scripts build", {
-        env: {
-            ...process.env,
-            DHIS2_BASE_URL: baseUrl,
-        },
-    });
 
-    gulp.task("bundle", () =>
+    await exec({
+        cmd: 'npx d2-app-scripts',
+        args: ['build'],
+        cwd: process.cwd(),
+        env: { ...process.env, DHIS2_BASE_URL: baseUrl, INLINE_RUNTIME_CHUNK: false, GENERATE_SOURCEMAP: false, SKIP_PREFLIGHT_CHECK: true },
+        pipe: false,
+    })
+
+    // Bundle all js chunks into a single file
+    gulp.task("bundle-js", () =>
         gulp
             .src("./build/app/static/**/*.js")
             .pipe(concat("main.js"))
             .pipe(gulp.dest("./build/app/static/js"))
     );
 
+    // Bundle all css chunks into a single file
+    gulp.task("bundle-css", () =>
+        gulp
+            .src("./build/app/static/**/*.css")
+            .pipe(concatCss("main.css"))
+            .pipe(gulp.dest("./build/app/static/css"))
+    );
+
+    // Inline all css and js files
     gulp.task("inline", () =>
         gulp
             .src("./build/app/*.html")
@@ -29,12 +42,12 @@ const handler = async () => {
                     `<script src="./static/js/main.js" inline></script>`
                 )
             )
-            .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+            .pipe(replace(/\<link.*rel\=\"stylesheet\"\>/g, `<link href="./static/css/main.css" rel="stylesheet" inline>`))
             .pipe(inlineSource({ compress: false, ignore: ["png"] }))
             .pipe(gulp.dest("./build/bundle"))
     );
 
-    await gulp.series("bundle", "inline")();
+    await gulp.series("bundle-js", "bundle-css", "inline")();
 };
 
 module.exports = {
